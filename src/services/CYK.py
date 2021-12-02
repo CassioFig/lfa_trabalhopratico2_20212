aa4a254e0f3fb94c525f18ee3e407a3c059893d5
Cássio Figueiredo <cassio.figueiredof@gmail.com>
2021-12-02 00:27:23 -0300

    Adição de explicação no código

--- src/services/CYK.py
+++ src/services/CYK.py
@@ -23,14 +23,21 @@ class CYK:
         for i in range(length):
             table.append([[] for j in range(length)])
 
+        print('--' * 14)
+        print('   Etapa: 1')
+        print('--' * 14)
         for i in range(length):
             for variable, values in self.__grammar.items():
                 if string[i] in values:
                     table[i][i].append(variable)
+                    print(f'A variável {variable} contém a letra {string[i]}, posição (1, {i + 1}): {self.__removeRepeated(table[i][i])}')
         return table
 
     def __execute(self, table, length: int):
         for i in range(1, length):
+            print('--' * 14)
+            print(f'   Etapa: {i + 1}')
+            print('--' * 14)
             for j in range(length - i):
                 for k in range(i):
                     for variable, values in self.__grammar.items():
@@ -38,6 +45,7 @@ class CYK:
                             if len(value) == 2:
                                 if value[0] in table[j][j+k] and value[1] in table[j+k+1][j+i]:
                                     table[j][j+i].append(variable)
+                                    print(f'A variação {value} está presente em {variable}, posição ({j + 1},{(j+i) + 1}): {self.__removeRepeated(table[j][j+i])}')
         return table
 
     def __isValid(self, table):
@@ -45,3 +53,10 @@ class CYK:
         if firstVariable in table[0][-1]:
             return True
         return False
+
+    def __removeRepeated(self, array):
+        aux = []
+        for value in array:
+            if value not in aux:
+                aux.append(value)
+        return aux
