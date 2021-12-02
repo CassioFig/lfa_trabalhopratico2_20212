fd91fb4d66cc9884fa1b6b5e9659f7a9109d9033
Cássio Figueiredo <cassio.figueiredof@gmail.com>
2021-12-02 20:30:34 -0300

    Update CYK.py

--- src/services/CYK.py
+++ src/services/CYK.py
@@ -24,19 +24,19 @@ class CYK:
             table.append([[] for j in range(length)])
 
         print('--' * 14)
-        print('   Etapa: 1')
+        print('   Etapa: 1 - Procurar na gramática quem gera os valores da 1 linha')
         print('--' * 14)
         for i in range(length):
             for variable, values in self.__grammar.items():
                 if string[i] in values:
                     table[i][i].append(variable)
-                    print(f'A variável {variable} contém a letra {string[i]}, posição (1, {i + 1}): {self.__removeRepeated(table[i][i])}')
+                    print(f'A letra {string[i]} está presente na variável {variable}, posição (1, {i + 1}): {self.__removeRepeated(table[i][i])}')
         return table
 
     def __execute(self, table, length: int):
         for i in range(1, length):
             print('--' * 14)
-            print(f'   Etapa: {i + 1}')
+            print(f'   Etapa: {i + 1} - Procurar na gramática as combinações de variáveis')
             print('--' * 14)
             for j in range(length - i):
                 for k in range(i):
